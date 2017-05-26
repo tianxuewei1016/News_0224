deb962af643cb90774c7a88937c8ef79d943f52d
tianxuewei1016 <93226539@qq.com>
2017-05-26 21:27:10 +0800

    新闻中心数据请求

--- app/src/main/java/com/atguigu/news_0224/newscenter/view/NewsCenterPager.java
+++ app/src/main/java/com/atguigu/news_0224/newscenter/view/NewsCenterPager.java
@@ -7,6 +7,11 @@ import android.view.Gravity;
 import android.widget.TextView;
 
 import com.atguigu.news_0224.base.BasePager;
+import com.atguigu.news_0224.utils.Constants;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * 作者：田学伟 on 2017/5/25 20:00
@@ -22,7 +27,7 @@ public class NewsCenterPager extends BasePager {
     @Override
     public void initData() {
         super.initData();
-        Log.e("TAG","新闻页面加载数据了");
+        Log.e("TAG", "新闻页面加载数据了");
         //设置标题
         tv_title.setText("新闻");
         //实例化视图
@@ -34,5 +39,36 @@ public class NewsCenterPager extends BasePager {
 
         //和父类的FrameLayout结合
         fl_main.addView(textView);
+        //网络请求
+        getDataFromNet();
+    }
+
+    /**
+     * 网络请求数据
+     */
+    private void getDataFromNet() {
+        //联网地址
+        RequestParams params = new RequestParams(Constants.NEWSCENTER_PAGER_URL);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.e("TAG", "请求成功==" + result);
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e("TAG", "请求失败==" + ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/atguigu/news_0224/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.atguigu.news_0224.utils;
+
+/**
+ * 作者：田学伟 on 2017/5/26 21:23
+ * QQ：93226539
+ * 作用：联网的地址
+ */
+
+public class Constants {
+
+    public static final String BASE_URL = "http://182.92.5.3/web_home";
+
+    /**
+     * 新闻中心的网络地址
+     */
+    public static final String NEWSCENTER_PAGER_URL = BASE_URL + "/static/api/news/categories.json";
+}
