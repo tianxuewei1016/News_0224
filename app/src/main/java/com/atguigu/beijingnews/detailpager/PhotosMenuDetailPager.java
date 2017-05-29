e7408e38302abcdfc24a5215b72c79f20e330c11
tianxuewei1016 <93226539@qq.com>
2017-05-29 21:15:16 +0800

    SwipeRefreshLayout下拉刷新

--- app/src/main/java/com/atguigu/beijingnews/detailpager/PhotosMenuDetailPager.java
+++ app/src/main/java/com/atguigu/beijingnews/detailpager/PhotosMenuDetailPager.java
@@ -1,6 +1,8 @@
 package com.atguigu.beijingnews.detailpager;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -36,6 +38,8 @@ public class PhotosMenuDetailPager extends MenuDetailBasePager {
     private final NewsCenterBean.DataBean dataBean;
     @InjectView(R.id.recyclerview)
     RecyclerView recyclerview;
+    @InjectView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout swipe_refresh_layout;
     private String url;
     private PhotosMenuDetailPagerAdapter adapter;
 
@@ -49,7 +53,21 @@ public class PhotosMenuDetailPager extends MenuDetailBasePager {
     @Override
     public View initView() {
         View view = View.inflate(mContext, R.layout.photos_menudetail_pager, null);
-        ButterKnife.inject(this,view);
+        ButterKnife.inject(this, view);//swipe_refresh_layout被实例化了
+
+        //设置下拉多少距离起作用
+        swipe_refresh_layout.setDistanceToTriggerSync(100);//设置下拉的距离
+
+        swipe_refresh_layout.setColorSchemeColors(Color.BLUE, Color.RED);//设置不同颜色
+        //设置背景的颜色
+        swipe_refresh_layout.setProgressBackgroundColorSchemeResource(android.R.color.holo_blue_bright);
+        //设置下拉刷新的监听
+        swipe_refresh_layout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getDataFromNet(url);
+            }
+        });
         return view;
     }
 
@@ -57,7 +75,7 @@ public class PhotosMenuDetailPager extends MenuDetailBasePager {
     public void initData() {
         super.initData();
         url = Constants.BASE_URL + dataBean.getUrl();
-        Log.e("TAG","图组的网络地址====="+url);
+        Log.e("TAG", "图组的网络地址=====" + url);
         getDataFromNet(url);
     }
 
@@ -66,8 +84,9 @@ public class PhotosMenuDetailPager extends MenuDetailBasePager {
         x.http().get(params, new Callback.CommonCallback<String>() {
             @Override
             public void onSuccess(String result) {
-                Log.e("TAG","图组数据请求成功==");
+                Log.e("TAG", "图组数据请求成功==");
                 processData(result);
+                swipe_refresh_layout.setRefreshing(false);
             }
 
             @Override
@@ -88,27 +107,27 @@ public class PhotosMenuDetailPager extends MenuDetailBasePager {
     }
 
     private void processData(String json) {
-        PhotosMenuDetailPagerBean bean = new Gson().fromJson(json,PhotosMenuDetailPagerBean.class);
-        Log.e("TAG","数组解析数据成功======"+ bean.getData().getNews().get(0).getTitle());
+        PhotosMenuDetailPagerBean bean = new Gson().fromJson(json, PhotosMenuDetailPagerBean.class);
+        Log.e("TAG", "数组解析数据成功======" + bean.getData().getNews().get(0).getTitle());
         //设置RecyclerView的适配器
-        adapter = new PhotosMenuDetailPagerAdapter(mContext,bean.getData().getNews());
+        adapter = new PhotosMenuDetailPagerAdapter(mContext, bean.getData().getNews());
         recyclerview.setAdapter(adapter);
 
         //设置布局管理器
-        recyclerview.setLayoutManager(new LinearLayoutManager(mContext,LinearLayoutManager.VERTICAL,false));
+        recyclerview.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
     }
 
     private boolean isList = true;
 
-    public void swichListGrid(ImageButton ib_swich_list_gird){
-        if(isList) {
+    public void swichListGrid(ImageButton ib_swich_list_gird) {
+        if (isList) {
             isList = false;
             //设置布局管理器
-            recyclerview.setLayoutManager(new GridLayoutManager(mContext,2,GridLayoutManager.VERTICAL,false));
+            recyclerview.setLayoutManager(new GridLayoutManager(mContext, 2, GridLayoutManager.VERTICAL, false));
             //按钮设置List效果
             ib_swich_list_gird.setImageResource(R.drawable.icon_pic_list_type);
-        }else{
-            recyclerview.setLayoutManager(new LinearLayoutManager(mContext,LinearLayoutManager.VERTICAL,false));
+        } else {
+            recyclerview.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
             isList = true;
             //按钮设置Grid效果
             ib_swich_list_gird.setImageResource(R.drawable.icon_pic_grid_type);
